021eba9e71d4512efb89d1cc36e5349608669edc
Lorenzo Iacopino <iacopino.1965855@studenti.uniroma1.it>
2024-10-05 21:46:08 +0200

    HMW1 init

---
+++ HMW1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HMW1 {
+    class Program {
+        static bool penetrates(float p){
+            return (new Random().Next(0, 100)<(p*100))? true:false;
+        }
+        static void print(int[] a, int n){
+            for (int i=1; i<n+1; i++) Console.WriteLine("Attacker "+i+": "+a[i-1]+" penetrated servers");
+        }
+        static void Main(string[] args){
+            int n;          // number of servers
+            int m;          // number of attackers
+            float p;        // probability of success
+            int[] fd;       // array of frequency distribution
+            n=100;
+            m=10;
+            p=0.7f;
+            fd=new int[m];
+            int temp;
+            for (int i=0; i<m; i++){
+                temp=0;
+                for (int j=0; j<n; j++) if (penetrates(p)) temp++;
+                fd[i]=temp;
+            }
+            print(fd, m);
+        }
+    }
+}
